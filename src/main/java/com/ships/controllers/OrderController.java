e31e1d26970169765820cef235325ce4a3df3561
ZqhKHP <G00336837@gmit.ie>
2018-04-13 22:33:33 +0100

    show orders working

--- src/main/java/com/ships/controllers/OrderController.java
+++ src/main/java/com/ships/controllers/OrderController.java
@@ -1,5 +1,31 @@
 package com.ships.controllers;
 
+import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.ships.model.OrderInfo;
+import com.ships.services.OrderService;
+
+@Controller
 public class OrderController {
+	
+	@Autowired
+	private OrderService orderService;
+	
+	@RequestMapping(value = "/showOrders", method = RequestMethod.GET)
+	public String getOrders(Model m) {
+
+		ArrayList<OrderInfo> orders = orderService.listAll();
+		m.addAttribute("orders", orders);
+
+		return "showOrders";
+	}//end getShips
 
 }
