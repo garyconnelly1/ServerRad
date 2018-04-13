bbd7d3f54f536aecb61af0a0aa777d78529af426
ZqhKHP <G00336837@gmit.ie>
2018-04-13 19:15:12 +0100

    add shipping company started

--- src/main/java/com/ships/controllers/ShipCompanyController.java
+++ src/main/java/com/ships/controllers/ShipCompanyController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.ships.model.Ship;
 import com.ships.model.ShippingCompany;
 import com.ships.services.ShippingCompanyService;
 
@@ -28,5 +29,26 @@ public class ShipCompanyController {
 
 		return "showShippingCompanies";
 	}//end getShips
+	
+	@RequestMapping(value = "/addShippingCompany", method = RequestMethod.GET)
+	public String getShip(@ModelAttribute("shipAdd") Ship s, HttpServletRequest h) {
+		return "addShippingCompany";
+	}//end getShip
+	
+	@RequestMapping(value = "/addShip", method = RequestMethod.POST)
+	public String addShip(@Valid @ModelAttribute("shipAdd") Ship s, BindingResult result, HttpServletRequest h, Model m) {
+		
+		if (result.hasErrors()) {
+			return "addShip";
+		} else {
+			shipService.addShip(s);
+			
+			ArrayList<Ship> ships = shipService.listAll();
+	
+			m.addAttribute("ships", ships);
+			//once the ship has been successfully added, redirect them to the show ships page to see the ship they added
+			return "showShips";
+		}
+	}//end addShip method
 
 }
