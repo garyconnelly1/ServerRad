f723c7a050ff4b4ebfe1fc265c0a9795bd43648c
ZqhKHP <G00336837@gmit.ie>
2018-04-13 23:05:38 +0100

    problem with the dropdowns

--- src/main/java/com/ships/controllers/OrderController.java
+++ src/main/java/com/ships/controllers/OrderController.java
@@ -28,11 +28,27 @@ public class OrderController {
 
 		return "showOrders";
 	}//end getShips
-	/*
-	@RequestMapping(value = "/addShip", method = RequestMethod.GET)
-	public String getShip(@ModelAttribute("shipAdd") Ship s, HttpServletRequest h) {
-		return "addShip";
+	
+	@RequestMapping(value = "/createOrder", method = RequestMethod.GET)
+	public String getOrder(@ModelAttribute("orderAdd") OrderInfo o, HttpServletRequest h) {
+		return "createOrder";
 	}//end getShip
-	*/
+	
+	@RequestMapping(value = "/createOrder", method = RequestMethod.POST)
+	public String addShip(@Valid @ModelAttribute("orderAdd") OrderInfo o, BindingResult result, HttpServletRequest h, Model m) {
+		
+		if (result.hasErrors()) {
+			return "createOrder";
+		} else {
+			orderService.addOrder(o);
+			
+			ArrayList<OrderInfo> orders = orderService.listAll();
+	
+			m.addAttribute("orders", orders);
+			//once the ship has been successfully added, redirect them to the show ships page to see the ship they added
+			return "showOrders";
+		}
+	}//end addShip method
+	
 
 }
