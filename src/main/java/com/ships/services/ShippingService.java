a228b889500252b3980e3e7f144b2dbfb457177a
ZqhKHP <G00336837@gmit.ie>
2018-04-13 16:23:05 +0100

    adding services package and data access class

---
+++ src/main/java/com/ships/services/ShippingService.java
@@ -0,0 +1,21 @@
+package com.ships.services;
+
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ships.model.Ship;
+import com.ships.repositories.ShippingInterface;
+
+@Service
+public class ShippingService {
+	
+	@Autowired
+	private ShippingInterface shipInterface;
+	
+	//access the db
+	public ArrayList<Ship> listAll() {
+		return (ArrayList<Ship>) shipInterface.findAll();
+	}
+
+}
