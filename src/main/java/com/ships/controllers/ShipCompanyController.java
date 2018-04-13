d45e6e8b93bb823f731b99e3504e1621b7d0d4e1
ZqhKHP <G00336837@gmit.ie>
2018-04-13 17:49:07 +0100

    company controller class

---
+++ src/main/java/com/ships/controllers/ShipCompanyController.java
@@ -0,0 +1,16 @@
+package com.ships.controllers;
+
+import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class ShipCompanyController {
+
+}
